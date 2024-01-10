a0f8836fccd3d5e84d0d0cf7fe4ad3f091fdab4a
Bassem S.Mosaad <bassemsaeed710@gmail.com>
2024-01-10 17:40:46 +0200

    ,,

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+
 def common_elements(set_1, set_2):
-    """Return a set of common elements between set_1 and set_2."""
-    return set_1.intersection(set_2)
+    """
+    A function that returns a set of
+    common elements in two sets
+    """
+    return(set_1 & set_2)
